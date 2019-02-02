6efe43f87e4da323080dfac6fc91c6e28bc74343
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-02-02 14:46:15 -0600

    added featured image support, registered nav menus and changed excerpt.

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// LOAD CSS
 function rosa_load_stylesheets() {
   wp_register_style("normalize", get_template_directory_uri() . "/inc/normalize.css");
   wp_enqueue_style("normalize");
@@ -9,8 +10,31 @@ function rosa_load_stylesheets() {
 }
 add_action("wp_enqueue_scripts", "rosa_load_stylesheets");
 
+// LOAD JAVASCRIPT
 function rosa_load_js() {
   wp_register_script("mainjs", get_template_directory_uri() . "/js/index.js", "", array(), true);
   wp_enqueue_script("mainjs");
 }
 add_action("wp_enqueue_scripts", "rosa_load_js");
+
+// ADD FEATURED IMAGE SUPPORT
+add_theme_support("post-thumbnails");
+// ADD MENU SUPPORT
+add_theme_support("menus");
+
+register_nav_menus(
+  array(
+    "primary" => __("Primary Menu", "theme")
+  )
+);
+
+// CHANGE EXCERPT LENGTH
+function rosa_excerpt_length($length) {
+  return 26;
+}
+add_filter("excerpt_length", "rosa_excerpt_length");
+
+function rosa_excerpt_readmore($more) {
+  return "...";
+}
+add_filter("excerpt_more", "rosa_excerpt_readmore");
