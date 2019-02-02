2358802d78056203d987605528d8ad08a556412b
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-02-02 14:44:11 -0600

    loop through posts

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php get_header(); ?>
+<?php while( have_posts() ) : the_post() ?>
+  <article>
+    <header>
+      <h2><?php the_title(); ?></h2>
+    </header>
+    <?php the_excerpt(); ?>
+    <?php the_post_thumbnail("medium");?>
+  </article>
+<?php endwhile; ?>
 <?php get_footer(); ?>
