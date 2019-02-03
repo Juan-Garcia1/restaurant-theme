9c052a052d6b7099b2228879164f5eeed92b441d
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-02-03 13:50:00 -0600

    update index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php get_header(); ?>
-<?php while( have_posts() ) : the_post() ?>
-  <article>
-    <header>
-      <h2><?php the_title(); ?></h2>
-    </header>
-    <?php the_excerpt(); ?>
-    <?php the_post_thumbnail("medium");?>
-  </article>
-<?php endwhile; ?>
+<?php if( have_posts() ) : ?>
+  <?php while( have_posts() ) : the_post() ?>
+    <article>
+      <header>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      </header>
+      <?php the_excerpt(); ?>
+      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail("medium");?></a>
+    </article>
+  <?php endwhile; ?>
+  <?php else : ?>
+    <p>There are no posts.</p>
+<?php endif; ?>
 <?php get_footer(); ?>
