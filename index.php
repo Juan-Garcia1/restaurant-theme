eaa3f359349529abec394c1388360bc3cd662df7
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-02-05 10:15:02 -0600

    update layout

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php get_header(); ?>
-<?php if( have_posts() ) : ?>
-  <?php while( have_posts() ) : the_post() ?>
-    <article>
-      <header>
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      </header>
-      <?php the_excerpt(); ?>
-      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail("medium");?></a>
-    </article>
-  <?php endwhile; ?>
-  <?php else : ?>
-    <p>There are no posts.</p>
-<?php endif; ?>
+  <main>
+    <div class="container">
+      <?php if( have_posts() ) : ?>
+        <?php while( have_posts() ) : the_post() ?>
+          <article class="row">
+            <div class="col text-center">
+              <header>
+              <p class="post-date"><?php the_date("M • nS • Y"); ?></p>
+                <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+              </header>
+              <?php the_excerpt(); ?>
+              <a href="<?php the_permalink(); ?>">read more</a>
+            </div>
+            <div class="col double-col">
+              <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail();?></a>
+            </div>
+          </article>
+        <?php endwhile; ?>
+        <?php else : ?>
+          <p>There are no posts.</p>
+      <?php endif; ?>
+    </div>
+  </main>
 <?php get_footer(); ?>
