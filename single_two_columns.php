914b4524832614a7a9e0d57d8a76cf5e00c435d2
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-02-17 12:24:58 -0600

    show tags for single post

--- single_two_columns.php
+++ single_two_columns.php
@@ -11,6 +11,9 @@ Template Post Type: post
       <h1><?php the_title(); ?></h1>
       <?php the_post_thumbnail("large"); ?>
       <?php the_content(); ?>
+      <?php if( the_tags() ) : ?>      
+        <?php the_tags(); ?>
+      <?php endif; ?>
     <?php endwhile; ?>
   </div>
   <?php get_sidebar(); ?>
