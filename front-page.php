5df7e6677ba0fc01955485671b80b5c8320cd89c
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-02-20 18:37:40 -0600

    front page

--- front-page.php
+++ front-page.php
@@ -1,3 +1,45 @@
 <?php get_header(); ?>
-<h1><?php echo "hi"; ?></h1>
+<div class="hero">
+  <div class="container"></div>
+</div>
+
+<div class="container">
+  <section>
+    <div class="row">
+      <div class="col"></div>
+      <div class="col"></div>
+    </div>
+  </section>
+</div>
+
+<section>
+  <div>
+    <h2><span>tasteful</span><br/>recipes</h2>
+  </div>
+</section>
+
+<div class="container">
+  <section>
+    <div class="row">
+      <div class="col"></div>
+      <div class="col"></div>
+    </div>
+  </section>
+</div>
+
+<section>
+  <div>
+    <h2><span>the perfect</span><br/>blend</h2>
+  </div>
+</section>
+
+<div class="container">
+  <section>
+    <div class="row">
+      <div class="col"></div>
+      <div class="col"></div>
+    </div>
+  </section>
+</div>
+
 <?php get_footer(); ?>
