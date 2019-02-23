9617ee32bdb508406f3c58bba7389e668bbcfe09
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-02-23 12:58:05 -0600

    add about description and social media links

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
     </main>
+    <footer>
+      <h4>about rosa</h4>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia nesciunt reprehenderit possimus saepe sunt dolorem odio labore accusamus, sint tempore.</p>
+      <h4>follow along</h4>
+      <div class="social-links">
+        <a href="#" class="social-links__link"><i class="fa"></i></a>
+        <a href="#" class="social-links__link"><i class="fa"></i></a>
+        <a href="#" class="social-links__link"><i class="fa"></i></a>
+        <a href="#" class="social-links__link"><i class="fa"></i></a>
+      </div>
+    </footer>
     <?php wp_footer(); ?>
   </body>
 </html>
