70f7a3a6bad03389614980a2edd6df249062f01d
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-02-23 12:46:52 -0600

    updated front page content

--- front-page.php
+++ front-page.php
@@ -6,40 +6,94 @@
 <div class="container">
   <section>
     <div class="row">
-      <div class="col"></div>
-      <div class="col"></div>
+      <div class="col">
+        <div class="title">
+          <h2 class="title__secondary">Discover</h2>
+          <h1 class="title__primary title__primary--black">our story</h1>
+        </div>
+        <!-- title end -->
+        <p>Rosa is a restaurant, bar and coffee roastery located on a busy corner in Farringdon's Exmouth Market. With glazed frontage on two sides of the building, overlooking the market and bustling London intersection.</p>
+        <a href="#">about us</a>
+      </div>
+      <!-- col end -->
+      <div class="col">
+        <img src="" alt="image 1 goes here">
+        <img src="" alt="image 2 goes here">
+      </div>
     </div>
+    <!-- row end -->
   </section>
 </div>
+<!-- container end -->
 
 <section>
-  <div>
-    <h2><span>tasteful</span><br/>recipes</h2>
+  <div class="center">
+    <div class="title">
+      <h2 class="title__secondary">Tasteful</h2>
+      <h1 class="title__primary title__primary--white">recipes</h1>
+    </div>
+    <!-- title end -->
   </div>
 </section>
 
 <div class="container">
   <section>
     <div class="row">
-      <div class="col"></div>
-      <div class="col"></div>
+      <div class="col">
+      <img src="" alt="image 1 goes here">
+      <img src="" alt="image 2 goes here">
+      <img src="" alt="image 3 goes here">
+      <img src="" alt="image 4 goes here">
+      </div>
+      <!-- col end -->
+      <div class="col">
+        <div class="title">
+          <h2 class="title__secondary">Discover</h2>
+          <h1 class="title__primary title_primary--black">menu</h1>
+        </div>
+        <!-- title end -->
+        <p>For those with pure food indulgence in mind, come next door and state your desires with our ever changing internationally and seasonally inspired samll plates. We love food, lots of different food, just like you.</p>
+        <a href="#">view the full menu</a>
+      </div>
+      <!-- col end -->
     </div>
+    <!-- row end -->
   </section>
 </div>
+<!-- container end -->
 
 <section>
-  <div>
-    <h2><span>the perfect</span><br/>blend</h2>
+  <div class="center">
+    <div class="title">
+      <h2 class="title__secondary">The perfect</h2>
+      <h1 class="title__primary title__primary--white">blend</h1>
+    </div>
+    <!-- title end -->
   </div>
 </section>
 
 <div class="container">
   <section>
     <div class="row">
-      <div class="col"></div>
-      <div class="col"></div>
+      <div class="col">
+        <div class="title">
+          <h2 class="title__secondary">Culinary</h2>
+          <h1 class="title__primary title__primary--black">delight</h1>
+        </div>
+        <!-- title end -->
+        <p>We promise an intimate and relaxed dining experience that offers something different to local and foreign patrons and ensures you enjoy a memorable food experience every time.</p>
+        <a href="#">make a reservation</a>
+      </div>
+      <!-- col end -->
+      <div class="col">
+        <img src="" alt="image1 goes here">
+        <img src="" alt="image 2 goes here">
+      </div>
+      <!-- col end -->
     </div>
+    <!-- row end -->
   </section>
 </div>
+<!-- container end -->
 
 <?php get_footer(); ?>
