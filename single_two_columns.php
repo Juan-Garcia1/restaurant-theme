0f5960644cc93c1874a170b863884a87c643d7bd
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-03-11 14:00:17 -0500

    style single post - two column layout template

--- single_two_columns.php
+++ single_two_columns.php
@@ -6,15 +6,25 @@ Template Post Type: post
 ?>
 
 <?php get_header(); ?>
-  <div class="content">
-    <?php while( have_posts() ) : the_post() ?>
-      <h1><?php the_title(); ?></h1>
-      <?php the_post_thumbnail("large"); ?>
-      <?php the_content(); ?>
-      <?php if( the_tags() ) : ?>      
-        <?php the_tags(); ?>
-      <?php endif; ?>
-    <?php endwhile; ?>
+  <div class="container">
+    <div class="grid">
+      <div class="content">
+        <?php while( have_posts() ) : the_post() ?>
+          <h1 class="uppercase"><?php the_title(); ?></h1>
+          <hr>
+          <div class="featured-img">
+            <?php the_post_thumbnail(); ?>
+          </div>
+          <?php the_content(); ?>
+          <?php if( the_tags() ) : ?>      
+            <?php the_tags(); ?>
+          <?php endif; ?>
+        <?php endwhile; ?>
+      </div>
+      <!-- content -->
+      <?php get_sidebar(); ?>
+    </div>
+    <!-- grid -->
   </div>
-  <?php get_sidebar(); ?>
+  <!-- container -->
 <?php get_footer(); ?>
