5f16c95ea17285bf12f956bcebd465e01ef024f6
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-04-11 14:44:27 -0500

    styled main header, posts and sidebar

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 </head>
 <body <?php body_class(); ?>>
   
-  <header>
+  <header id="main-header">
     <div class="container">
       <div>
         <?php if(has_custom_logo()) : ?>
--- index.php
+++ index.php
@@ -1,16 +1,23 @@
+<?php 
+$the_query = new WP_Query( array(
+  "post_type" => "post",
+  "order" => "ASC"
+) ); 
+?>
+
 <?php get_header(); ?>
   <main>
     <div class="container">
-      <?php if( have_posts() ) : ?>
-        <?php while( have_posts() ) : the_post() ?>
-          <article class="row">
+      <?php if( $the_query->have_posts() ) : ?>
+        <?php while( $the_query->have_posts() ) : $the_query->the_post() ?>
+          <article class="post row">
             <div class="col text-center mb-md">
               <header>
               <p class="post-date"><?php the_date("M • nS • Y"); ?></p>
                 <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
               </header>
               <?php the_excerpt(); ?>
-              <a href="<?php the_permalink(); ?>">read more</a>
+              <a class="post-link" href="<?php the_permalink(); ?>">read more</a>
             </div>
             <div class="col double-col">
               <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail();?></a>
